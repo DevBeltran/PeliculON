287ba57f30d290a6007afbf3b6a0991f1b9be07e
Pedro <developerpgb@gmail.com>
2023-03-21 19:19:17 +0000

    Creamos el ViewModel

---
+++ PeliculOn/MoviesVM.swift
@@ -0,0 +1,13 @@
+//
+//  MoviesVM.swift
+//  PeliculOn
+//
+//  Created by Pedro  on 21/3/23.
+//
+
+import Foundation
+
+final class MoviesVM:ObservableObject {
+    @Published var movies:[MoviesResult] = []
+    
+}
