74821ec03248947cba33b62f762668699f73b7e7
Pedro <developerpgb@gmail.com>
2023-03-21 10:07:37 +0000

    Initial Commit

---
+++ PeliculOn/NetworkInterface.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkInterface.swift
+//  PeliculOn
+//
+//  Created by Pedro  on 21/3/23.
+//
+
+import Foundation
---
+++ PeliculOn/PeliculOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeliculOnApp.swift
+//  PeliculOn
+//
+//  Created by Pedro  on 21/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PeliculOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
